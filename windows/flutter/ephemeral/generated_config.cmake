1e335aa0f48bb14c40de974f6f46859dfe5f8a63
MrRoboto9014 <david.alejandro.ortega301@gmail.com>
2026-06-09 23:57:18 -0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "A:\\Programacion\\VSCode Files\\FlutterAPI\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\david\\Downloads\\Proyecto_Final_DavidAlejandroOrtegaGarcia\\Final_Activity_Decompile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=A:\\Programacion\\VSCode Files\\FlutterAPI\\flutter"
+  "PROJECT_DIR=C:\\Users\\david\\Downloads\\Proyecto_Final_DavidAlejandroOrtegaGarcia\\Final_Activity_Decompile"
+  "FLUTTER_ROOT=A:\\Programacion\\VSCode Files\\FlutterAPI\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\david\\Downloads\\Proyecto_Final_DavidAlejandroOrtegaGarcia\\Final_Activity_Decompile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\david\\Downloads\\Proyecto_Final_DavidAlejandroOrtegaGarcia\\Final_Activity_Decompile"
+  "FLUTTER_TARGET=lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=C:\\Users\\david\\Downloads\\Proyecto_Final_DavidAlejandroOrtegaGarcia\\Final_Activity_Decompile\\.dart_tool\\package_config.json"
+)
